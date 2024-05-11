8dcd8d5ebbccb49059601032fbaf2fc0de3c0069
SudeepIter <nsudeep508@gmail.com>
2024-05-11 16:31:15 +0530

    Add files via upload

---
+++ A4Q7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class A4Q7 {
+public static void main(String[] args) {
+    int a,i,exp=1;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("19 ka phada sunana:");
+    a=sc.nextInt();
+    for (i=1;i<=10;i++)
+    {
+       
+        System.out.println(a+"X"+i+"="+(i*a));
+        
+
+    }
+
+
+}
+}
